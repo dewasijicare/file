04b2a6cbb5d9f35a1943f2aa6f363686f789de5d
dewasijicare <dewasijicare@gmail.com>
2025-11-02 08:37:20 +0700

    Update gavan.js

--- gavan.js
+++ gavan.js
@@ -306,7 +306,7 @@
     
     // [FUNGSI DIPERBARUI] Untuk inisialisasi input bet agar memiliki format
     function initializeBetFormatting() {
-        document.querySelectorAll('#betting-page-container td.td-input > input.form-control:not(.display-input)').forEach(originalInput => {
+        document.querySelectorAll('#betting-page-container td.td-input > input.form-control[name^="bet"]:not(.display-input), #betting-page-container td.td-input > input.form-control[id^="bet-"]:not(.display-input)').forEach(originalInput => {
             // Cek apakah input sudah diformat atau merupakan input tersembunyi
             if (originalInput.dataset.betFormatted === 'true' || originalInput.offsetParent === null || originalInput.type === 'hidden') return;
             
@@ -1456,4 +1456,5 @@
         }
     });
 })();
+
 
